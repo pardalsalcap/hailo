7a8cd1e3a070dff03d726560abcc6fa009e6861c
pardalsalcap <dani.casasnovas@gmail.com>
2024-02-05 11:22:40 +0100

    Format code

--- resources/views/livewire/auth/reset.blade.php
+++ resources/views/livewire/auth/reset.blade.php
@@ -37,8 +37,6 @@
                     <button type="submit" class="flex w-full justify-center rounded-md bg-hailo-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ __("hailo::hailo.reset_button") }}</button>
                 </div>
             </form>
-
-
         </div>
 
         <p class="mt-10 text-center text-sm text-gray-500">
